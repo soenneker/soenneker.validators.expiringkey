14fd6facdf131a436d34a92e163cab8c3dd0f72c
soenneker <jake@soenneker.com>
2026-08-29 14:17:26 -0500

    Improve public API XML documentation

--- src/Soenneker.Validators.ExpiringKey/Registrars/ExpiringKeyValidatorRegistrar.cs
+++ src/Soenneker.Validators.ExpiringKey/Registrars/ExpiringKeyValidatorRegistrar.cs
@@ -12,6 +12,8 @@ public static class ExpiringKeyValidatorRegistrar
     /// <summary>
     /// Adds <see cref="IExpiringKeyValidator"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddExpiringKeyValidatorAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<IExpiringKeyValidator, ExpiringKeyValidator>();
@@ -21,6 +23,8 @@ public static class ExpiringKeyValidatorRegistrar
     /// <summary>
     /// Adds <see cref="IExpiringKeyValidator"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddExpiringKeyValidatorAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<IExpiringKeyValidator, ExpiringKeyValidator>();
